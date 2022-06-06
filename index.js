5a977511d583fc8804597f37fe361edda8bfd396
Jobsidney <jobsidney67@gmail.com>
2022-06-06 15:12:28 +0300

    commit tree 5af4d52a374bc2b6d49345a3742fad6e553608e8

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let num3=4
+const num4=34
+const myArray=(1,4,7,20,19);
+const multiply=num1*num2;
+const random=Math.ceil(Math.random());
+const max=Math.max(1,4,20,6);
+const mod=num4%5;
